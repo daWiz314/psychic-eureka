c94eb0e2373c71d122e39c2bdb43b073dd3d8f89
Harry <programmer6299@icloud.com>
2025-05-19 12:40:13 +0000

    Added data base set up Reconfigured success to display all data in table Reconfigured login to actually post data to table

--- index/blueprint_index.py
+++ index/blueprint_index.py
@@ -1,6 +1,24 @@
 # Main landing point for flask application
+import sqlite3
 
 from flask import Flask, Blueprint, redirect, url_for, request, render_template
+
+def set_up():
+    # Set up the database
+    db = sqlite3.connect('database.db', check_same_thread=False)
+    # Delete the table after every set up of server
+    db.execute('''
+                DROP TABLE IF EXISTS users
+                ''')
+    # Create new table with 2 columns
+    # Name and message
+    db.execute('''
+               CREATE TABLE IF NOT EXISTS users \
+               (name TEXT, message TEXT)
+               ''')
+    db.commit()
+    db.close()
+    print("Database set up complete.")
 
 index_blueprint = Blueprint('index',
                              __name__,
@@ -18,16 +36,31 @@ def index():
     return redirect('/')
 
 # Success for logging in, will update later
-@index_blueprint.route('/success/<name>')
-def success(name):
-    return render_template('/index/success.html', name=name)
+@index_blueprint.route('/success')
+def success():
+    connect = sqlite3.connect('database.db')
+    cur = connect.cursor()
+    # Get everything and pass it to webpage
+    cur.execute('''
+                SELECT * FROM users
+                ''')
+    data = cur.fetchall()
+    connect.close()
+
+    return render_template('/index/success.html', data=data)
 
 # Login route
 # Will update later to help against CSRF
 @index_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('index.success', name=user), code=301)
+        user = request.form['name']
+        message = request.form['message']
+        print(user)
+        with sqlite3.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute(''' INSERT INTO users (name, message) VALUES (?,?) ''', (user, message))
+            con.commit()
+        return redirect(url_for('index.success'), code=301)
     else:
-        return render_template('index.html')
+        return render_template('index.index.html')
