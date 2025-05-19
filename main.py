40ec11a2231a6475657b89af290ed1e9578adc0e
Harry <programmer6299@icloud.com>
2025-05-19 12:39:31 +0000

    Included databse set up

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, render_template
 from index.blueprint_index import index_blueprint
+from index.blueprint_index import set_up as index_set_up
 from e_com_ref.e_com_ref import ecom_ref
 
 # Main flask application
@@ -8,6 +9,8 @@ app = Flask(__name__)
 
 # Main index blueprint
 app.register_blueprint(index_blueprint, url_prefix='/index')
+# Set up the database
+index_set_up()
 
 # E-commerce reference blueprint
 app.register_blueprint(ecom_ref, url_prefix='/e_com_ref')
