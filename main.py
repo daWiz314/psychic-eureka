b8fc2318241d2836b96b840f967425aa01438728
Harry <programmer6299@icloud.com>
2025-05-19 03:34:16 +0000

    Added url_prefix for index

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask, render_template
 from index.blueprint_index import index_blueprint
 from e_com_ref.e_com_ref import ecom_ref
 
@@ -7,10 +7,14 @@ from e_com_ref.e_com_ref import ecom_ref
 app = Flask(__name__)
 
 # Main index blueprint
-app.register_blueprint(index_blueprint)
+app.register_blueprint(index_blueprint, url_prefix='/index')
 
 # E-commerce reference blueprint
 app.register_blueprint(ecom_ref, url_prefix='/e_com_ref')
+
+@app.route('/')
+def hello_world():
+    return render_template('index/index.html')
 
 # Start app
 if __name__ == '__main__':
